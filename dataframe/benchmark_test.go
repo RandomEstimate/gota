0ddc01f66343fdf2fdb59262e66a82a3992ae650
RandomEstimate <randomestimate@sina.com>
2023-12-16 22:22:45 +0800

    修改go.mod module

--- dataframe/benchmark_test.go
+++ dataframe/benchmark_test.go
@@ -5,8 +5,8 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/go-gota/gota/dataframe"
-	"github.com/go-gota/gota/series"
+	"github.com/RandomEstimate/gota/dataframe"
+	"github.com/RandomEstimate/gota/series"
 )
 
 func generateSeries(n, rep int) (data []series.Series) {
--- dataframe/examples_test.go
+++ dataframe/examples_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/go-gota/gota/dataframe"
-	"github.com/go-gota/gota/series"
+	"github.com/RandomEstimate/gota/dataframe"
+	"github.com/RandomEstimate/gota/series"
 )
 
 func ExampleNew() {
